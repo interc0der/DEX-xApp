98763d7e4c305618f126ae6d235cf260af91173e
KoenPaas <koen@paas.nl>
2021-07-06 17:22:49 +0200

    Use version check in plugin

--- src/components/Offers.vue
+++ src/components/Offers.vue
@@ -149,50 +149,11 @@ export default {
                 }
             }
         },
-        versionCheck(v1, v2) {
-            var v1parts = v1.split('.');
-            var v2parts = v2.split('.');
-
-            // First, validate both numbers are true version numbers
-            function validateParts(parts) {
-                for (var i = 0; i < parts.length; ++i) {
-                    if (!/^\d+$/.test(parts[i])) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            if (!validateParts(v1parts) || !validateParts(v2parts)) {
-                return NaN;
-            }
-
-            for (var i = 0; i < v1parts.length; ++i) {
-                if (v2parts.length === i) {
-                    return 1;
-                }
-
-                if (v1parts[i] === v2parts[i]) {
-                    continue;
-                }
-                if (v1parts[i] > v2parts[i]) {
-                    return 1;
-                }
-                return -1;
-            }
-
-            if (v1parts.length != v2parts.length) {
-                return -1;
-            }
-
-            return 0;
-        },
         async info(order) {
             const txId = order.PreviousTxnID || order.hash || order.Sequence
             try {
                 const data = await xapp.getTokenData()
-                if (this.versionCheck(data.version, '2.1.0') < 0) throw 'Update XUMM to use this feature'
-                if(data.nodetype === 'MAINNET') xapp.openExternalBrowser(`https://livenet.xrpl.org/transactions/${txId}`)
-                else if(data.nodetype === 'TESTNET') xapp.openExternalBrowser(`https://testnet.xrpl.org/transactions/${txId}`)
+                if (xapp.versionCheck(data.version, '2.1.0') < 0) throw 'Update XUMM to use this feature'
             } catch(e) {
                 this.$emitter.emit('modal', {
                     type: 'error',
--- src/plugins/xapp.js
+++ src/plugins/xapp.js
@@ -127,13 +127,52 @@ const payload = async (payload) => {
     }
 }
 
+const versionCheck = (v1, v2) => {
+    var v1parts = v1.split('.');
+    var v2parts = v2.split('.');
+
+    // First, validate both numbers are true version numbers
+    function validateParts(parts) {
+        for (var i = 0; i < parts.length; ++i) {
+            if (!/^\d+$/.test(parts[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    if (!validateParts(v1parts) || !validateParts(v2parts)) {
+        return NaN;
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length === i) {
+            return 1;
+        }
+
+        if (v1parts[i] === v2parts[i]) {
+            continue;
+        }
+        if (v1parts[i] > v2parts[i]) {
+            return 1;
+        }
+        return -1;
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    return 0;
+}
+
 export default {
     getTokenData,
     closeXapp,
     signPayload: payload,
     getCuratedAssets,
     openExternalBrowser,
-    openTxViewer
+    openTxViewer,
+    versionCheck
 }
 
 // export default {
