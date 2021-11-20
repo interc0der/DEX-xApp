65f6ee8c88e6b8c167a70b2df2946e8f21b0ef6a
KoenPaas <36813373+KoenPaas@users.noreply.github.com>
2021-11-20 15:48:57 +0100

    Don't show ticker if number is NaN

--- src/components/Ticker.vue
+++ src/components/Ticker.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ticker" :class="{'buy': Math.sign(Number(ticker)) > 0, 'sell': Math.sign(Number(ticker)) < 0}">
+    <div class="ticker" :class="{'buy': Math.sign(Number(ticker)) > 0, 'sell': Math.sign(Number(ticker)) < 0}" v-show="!isNaN(ticker)">
         <fa v-if="Math.sign(Number(ticker)) > 0" class="buy" :icon="['fa', 'caret-up']"/>
         <fa v-else-if="Math.sign(Number(ticker)) < 0" class="sell" :icon="['fa', 'caret-down']"/>
         <label class="number">{{ ticker }}%</label>
